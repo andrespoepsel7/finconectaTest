06eef461872ec7e3c7aac5f495fc63d2211ae580
andrespoepsel7 <andres.poepselv@gmail.com>
2023-12-11 15:04:36 -0400

    Created .env file to store credentials and improve application order

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
-export const PORT = 5555
+import dotenv from 'dotenv'
+dotenv.config()
 
-export const mongoDBURL = 'mongodb+srv://root:root@pepi01.x4m0lwi.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const PORT = 5555
+export const mongoDBURL = `mongodb+srv://root:${process.env.MONGODB_PASSWORD}@pepi01.x4m0lwi.mongodb.net/books-collection?retryWrites=true&w=majority`
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
-import express from "express"
+import express, { request } from "express"
 import { PORT, mongoDBURL } from "./config.js"
 import mongoose from "mongoose"
+import { Book } from "./bookModel.js"
 
 const app = express()
 
@@ -8,6 +9,16 @@ const app = express()
 app.get('/', (req, res)=>{
   console.log(req)
   return res.status(234).send("Hello World!")
+})
+
+app.post('/books', async (request, response) => {
+  try{
+    // Logic to create a new book based on it's fields
+
+  }catch(error){
+    console.log(error)
+    response.status(500).send({message:error.message})
+  }
 })
 
 
