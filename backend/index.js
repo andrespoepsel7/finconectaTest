1d11f4ac1b7f38d79e1d9ce05ff55c34e40a3433
andrespoepsel7 <andres.poepselv@gmail.com>
2023-12-11 18:30:05 -0400

    Crud functionality created, now working on refactoring code with express router

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
-import express, { request } from "express"
+import express, { request, response } from "express"
 import { PORT, mongoDBURL } from "./config.js"
 import mongoose from "mongoose"
 import { Book } from "./bookModel.js"
 
 const app = express()
+
+app.use(express.json())
 
 // 
 app.get('/', (req, res)=>{
@@ -14,8 +16,91 @@ app.get('/', (req, res)=>{
 app.post('/books', async (request, response) => {
   try{
     // Logic to create a new book based on it's fields
+    if(
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ){
+      return response.status(400).send({
+        message:'All fields are required: title, author,  publish year'
+      })
+    }
+    const newBook = {
+      title:request.body.title, 
+      author:request.body.author, 
+      publishYear:request.body.publishYear, 
+    }
+    const book = await Book.create(newBook)
+    return response.status(201).send(book)
 
   }catch(error){
+    console.log(error)
+    response.status(500).send({message:error.message})
+  }
+})
+
+// Get all books
+app.get('/books', async (request, response) => {
+  try {
+    const books = await Book.find({})
+    return response.status(200).json({
+      count:books.length,
+      data:books,
+    })
+
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).send({message:error.message})
+  }
+})
+
+// Get a book by id
+app.get('/books/:id', async (request, response) => {
+  try {
+    const {id} = request.params
+    const book = await Book.findById(id)
+    return response.status(200).json(book)
+
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).send({message:error.message})
+  }
+})
+
+// Update a book
+app.put('/books/:id', async (request, response)=>{
+  try {
+    if(
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ){
+      return response.status(400).send({
+        message:'All fields are required: title, author,  publish year'
+      })
+    }
+    const {id} = request.params
+    const result = await Book.findByIdAndUpdate(id, request.body)
+    if(!result){
+      return response.status(404).json({message:'Book not found'})
+    }
+    return response.status(200).send({message:'Book updated succesfully'})
+  } catch (error) {
+    console.log(error)
+    response.statusMessage(500).send({message:error.message})
+  }
+})
+
+// Delete book
+app.delete('/books/:id', async (request, response) => {
+  try {
+    const {id} = request.params
+    const result = await Book.findByIdAndDelete(id)
+    if(!result){
+      return response.status(404).json({message:'Book not found'})
+    }
+    return response.status(200).send({message:"Book deleted succesfully"})
+  } catch (error) {
     console.log(error)
     response.status(500).send({message:error.message})
   }
